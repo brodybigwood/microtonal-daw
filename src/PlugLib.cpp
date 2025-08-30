7d0203a0ba1fc22f960d066f6699eb64fb82004c
brodybigwood <brodybigwood@gmail.com>
2025-08-30 12:45:30 -0700

    only open library in vst implementation

--- src/PlugLib.cpp
+++ src/PlugLib.cpp
@@ -1,12 +1,7 @@
 #include "PlugLib.h"
 
 PlugLib::PlugLib(std::string path) : path(std::move(path)) {
-	handle = dlopen(path.c_str(), RTLD_NOW | RTLD_GLOBAL);
-	if(!handle) {
-		std::cerr << "dlopen failed: " << dlerror() << std::endl;
-	} else {
-		std::cout << "dlopen succeeded: handle=" << handle << std::endl;
-	}
+
 }
 
 PlugLib::~PlugLib() {
