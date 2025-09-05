adf3e6b8d87a3b9cd617550e2a238e944d51309e
brodybigwood <brodybigwood@gmail.com>
2025-09-05 14:16:38 -0700

    fixed rendering for volumebar and added color changing

--- src/VolumeBar.cpp
+++ src/VolumeBar.cpp
@@ -4,16 +4,25 @@
 VolumeBar::VolumeBar() {};
 
 void VolumeBar::render(SDL_Renderer* renderer, SDL_FRect* dstRect) {
-    SDL_SetRenderDrawColor(renderer, 255, 20, 20, 255);
-    float w = dstRect->w / 2;
-    float h = dstRect->h * (3 / 4.0f) * amplitude;
+    SDL_SetRenderDrawColor(renderer, 255 * amplitude, 255 * (1 - amplitude), 0, 255);
+    float x = dstRect->x + (dstRect->w / 10.0f);
+    float y = dstRect->y + (dstRect->h * 2 / 10.0f);
+    float w = dstRect->w * 8 / 10.0f;
+    float h = dstRect->h * 7 / 10.0f;
+
+    SDL_FRect bounds{ x, y, w, h };
+
     SDL_FRect volRect{
-        dstRect->x + (dstRect->w/2) - w/2,
-        dstRect->y + (dstRect->h/2) + (h/2) - h,
-        w,
-        h
+        bounds.x,
+        bounds.y + ( 1 - amplitude ) * bounds.h ,
+        bounds.w,
+        bounds.h * amplitude
     };
+
     SDL_RenderFillRect(renderer, &volRect);
+
+    SDL_SetRenderDrawColor(renderer, 30, 30, 30, 255);
+    SDL_RenderRect(renderer, &bounds);
 
 }
 
@@ -26,6 +35,10 @@ void VolumeBar::process(audioData data) {
         auto buffer = stream.channels[ch].buffer;
         for(size_t s = 0; s < data.bufferSize; s++) {
             float abs = std::fabs(buffer[s]);
+            if(abs > 1) {
+                amplitude = 1;
+                return;
+            }
             if( abs > amplitude ) amplitude = abs;
         }
     }
