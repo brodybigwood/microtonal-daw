94d313b9d941e08ec4918206d53fad15d078e0d5
brodybigwood <brodybigwood@gmail.com>
2026-02-10 15:37:45 -0800

    send mouse motion in bursts rather than hundred of events

--- src/WindowHandler.cpp
+++ src/WindowHandler.cpp
@@ -47,7 +47,7 @@ void WindowHandler::createPianoRoll(Region* region, SDL_FRect* pRect) {
     home->pianoRoll = new PianoRoll(pRect, region, &(home->pianoRollDetached));
 }
 
-bool WindowHandler::tick() {
+bool WindowHandler::tick() { 
 
     double timeSinceLastFrame = double(SDL_GetTicks())-double(lastTime);
     if(timeSinceLastFrame >= frameTime) {
@@ -55,9 +55,38 @@ bool WindowHandler::tick() {
 
         if (ctxMenu->active) home->render(); // need to render behind ctxmenu first
 
+        static SDL_Event motion;
+        if (motion.type != 0) {
+            if (ctxMenu->active) ctxMenu->tick(motion);
+            else if (!home->handleInput(motion)) return false;
+        }
+        SDL_zero(motion);
+
         SDL_Event e;
         while(SDL_PollEvent(&e)) {
-
+            if (e.type == SDL_EVENT_MOUSE_MOTION) {
+                if (motion.type == 0) { //
+                    motion.type = SDL_EVENT_MOUSE_MOTION;
+                    motion.motion.windowID = e.motion.windowID; // the accumulated motion will be for this window
+                }
+                if (motion.motion.windowID == e.motion.windowID) { // if the new event is for the accumulated event's window, accumulate it
+                    motion.motion.xrel += e.motion.xrel;
+                    motion.motion.yrel += e.motion.yrel;
+                } else { // if not, handle the accumulated motion for the previous window and then reset it
+                    if (ctxMenu->active) ctxMenu->tick(motion);
+                    else if (!home->handleInput(motion)) return false;
+                    SDL_zero(motion);
+                    motion.type = SDL_EVENT_MOUSE_MOTION; // accumulate the motion for the new window 
+                    motion.motion.windowID = e.motion.windowID;
+                    motion.motion.xrel += e.motion.xrel;
+                    motion.motion.yrel += e.motion.yrel;
+                }
+                continue; // e already saved or handled
+            } else if (motion.type != 0) { // if there is a non motion event but there is previous motion, handle the previous motion first
+                if (ctxMenu->active) ctxMenu->tick(motion);
+                else if (!home->handleInput(motion)) return false;
+                SDL_zero(motion);
+            }
             if (e.type == SDL_EVENT_MOUSE_BUTTON_DOWN) {
                 SDL_Window* clickedWindow = SDL_GetWindowFromID(e.button.windowID);
                 if (clickedWindow) {
