043cb6ecd233be849d1990d44c24a24b0cbf218e
brodybigwood <brodybigwood@gmail.com>
2026-02-26 17:48:14 -0800

    sdl quit

--- src/main.cpp
+++ src/main.cpp
@@ -44,6 +44,8 @@ int main() {
         std::this_thread::sleep_for(std::chrono::milliseconds(5)); // 1000ms / 200Hz = 5ms
     }
 
+    SDL_Quit();
+
     audioManager->stop();
     project->save();
 
